17945d80a69e0389445afa807d002787c8d34cd5
nico <nicolasdasilva317@gmail.com>
2017-03-01 16:13:51 +0100

    login page

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var hbs = require("hbs");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  hbs.registerPartial("popup_login", fs.readFileSync(__dirname+"/../views/login.hbs", 'utf-8'));
   res.render('index', { title: 'Express' });
 });
 
